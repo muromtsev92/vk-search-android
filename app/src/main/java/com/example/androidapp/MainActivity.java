c1097665541ecb411e61fdf13080112bbbbf18c1
SergeiMuromtsev <k29393@yandex.ru>
2023-09-28 01:06:34 +0500

    add simple list in text view

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView plList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_names);
+
+        String[] names = {"Java", "Kotlin", "C++"};
+        plList.setText("");
+        for(String name: names) {
+            plList.append(name + "\n");
+        }
     }
 }
